8d0dc96ecd893c56e68ad8575f8e8c5445337d7d
AM-77 <amine.griche77@gmail.com>
2019-10-27 00:09:46 +0200

    added redux

--- the-todo-app/src/index.js
+++ the-todo-app/src/index.js
@@ -5,7 +5,10 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { Provider } from "react-redux"
+import todoStore from "./todoStore"
+
+ReactDOM.render(<Provider store={todoStore}><App /></Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
