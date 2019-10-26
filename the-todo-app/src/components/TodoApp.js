2b932302d951892018fffeb0b19bcd471c99764e
AM-77 <amine.griche77@gmail.com>
2019-10-27 00:10:16 +0200

    added redux

--- the-todo-app/src/components/TodoApp.js
+++ the-todo-app/src/components/TodoApp.js
@@ -1,87 +1,69 @@
 import React, { Component } from "react"
 import TodoList from "./TodoList"
+import { connect } from "react-redux"
+import { addTodo, deleteTodo, sorteTodo, checkTodo } from "./../todoStore"
 
-export default class TodoApp extends Component {
+const mapStateToProps = (todoStore) => ({
+    todoList: todoStore.todo
+})
 
-    constructor(props){
-        super(props)
-        this.state = {
-            todoList: [{
-                    id: 1,
-                    content: "Add a README file.",
-                    checked: true
-                },
-                {
-                    id: 2,
-                    content: "Add a documentation.",
-                    checked: false
-                },
-                {
-                    id: 3,
-                    content: "Update the dependencies.",
-                    checked: true
-                }]
-        }
-    }
+const mapDispatchToProps = {
+    addTodo: addTodo,
+    deleteTodo: deleteTodo,
+    sorteTodo: sorteTodo,
+    checkTodo: checkTodo
+}
 
-    componentDidMount(){
-        this.sorteList()
+const connectStore = connect(mapStateToProps, mapDispatchToProps);
+
+const TodoApp = connectStore(class extends Component {
+
+    componentDidMount() {
+        this.props.sorteTodo()
     }
 
     onCheck = (id) => {
-        this.setState({ 
-            todoList: this.state.todoList.map((todoItem) => {
-                if(todoItem.id === id)
-                    todoItem.checked = !todoItem.checked
-                
-                return todoItem
-            })
-        }, () => this.sorteList())
+        this.props.checkTodo(id)
+        this.props.sorteTodo()
     }
 
     onDelete = (id) => {
-        this.setState({
-            todoList: this.state.todoList.filter( todoItem =>  todoItem.id !== id )  
-        })
+        this.props.deleteTodo(id)
     }
 
-    addItem = (e) => {
-        console.log(e.target);
-        
-        let newItem_content = e.target.parentElement.firstElementChild.value
+    addItem = () => {
+        let newItem_content = document.querySelector("input.add-todo").value
+        if (newItem_content !== "" && newItem_content !== undefined) {
+            this.props.addTodo({
+                id: new Date().getTime(),
+                content: newItem_content,
+                checked: false
+            })
 
-        if (newItem_content !== "" && newItem_content !== undefined ){
-            this.setState({
-                todoList: [...this.state.todoList, {
-                    id: this.state.todoList.length + 1 ,
-                    content: newItem_content,
-                    checked: false
-                }]
-            }, () => this.sorteList())
-        }        
+            this.props.sorteTodo()
+
+            document.querySelector("input.add-todo").value = null
+        }
+
     }
 
-    sorteList = () =>{
-        this.setState({
-            todoList: [...this.state.todoList.filter(item => !item.checked), ...this.state.todoList.filter(item => item.checked)]
-        })
-    }
-
-    render(){
+    render() {
         return (
             <div className="container">
                 <div className="header">
                     <h1>The-Todo-App</h1>
                     <div className="input">
-                        <input type="text" name="add-todo" id="add-todo" className="add-todo" placeholder="Add a todo"/>
+                        <input type="text" name="add-todo" id="add-todo" className="add-todo" placeholder="Add a todo" />
                         <button onClick={this.addItem} className="add-btn fa fa-plus"></button>
                     </div>
                 </div>
                 <div className="main">
-                    <TodoList onCheck={this.onCheck} onDelete={this.onDelete} todolist={this.state.todoList} />
+                    <TodoList onCheck={this.onCheck} onDelete={this.onDelete} todolist={this.props.todoList} />
                 </div>
                 <div className="footer"></div>
             </div>
         )
     }
-}
+})
+
+export default TodoApp
