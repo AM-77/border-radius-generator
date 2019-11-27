63bcb7c8a70df329cfa251e81938bdefd11351a2
AM-77 <amine.griche77@gmail.com>
2019-11-27 23:47:04 +0100

    added context provider

--- letra/src/App.js
+++ letra/src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, createContext } from 'react'
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import Home from "./components/Home"
+import Lyrics from "./components/Lyrics"
+import Artist from "./components/Artist"
+import Navbar from "./components/Navbar"
+import Search from './components/Search'
+import NotFound from "./components/NotFound"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export const URLContext = createContext('http://localhost:3300')
+
+class App extends Component {
+  render() {
+    return (
+      <URLContext.Provider >
+
+        <NotFound></NotFound>
+
+        <Router>
+          <div><Navbar /></div>
+          <div>
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/artist/:artist" component={Artist} />
+              <Route path="/lyrics/:artist/:track" component={Lyrics} />
+              <Route path="/search/:looking_for" component={Search} />
+              <Route component={NotFound} />
+            </Switch>
+          </div>
+        </Router>
+      </URLContext.Provider>
+    )
+  }
 }
 
-export default App;
+export default App
