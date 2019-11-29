685032efbcd261e399dac6e3a6bd02b84031e71c
AM-77 <amine.griche77@gmail.com>
2019-11-29 16:38:11 +0100

    added 'Enter' searching

--- letra/src/components/Navbar.js
+++ letra/src/components/Navbar.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react'
 import { Redirect } from 'react-router-dom'
 
+import { ReactComponent as Loupe } from "../loupe.svg";
 export default class Navbar extends Component {
 
     constructor(props) {
         super(props)
         this.state = {}
+    }
+
+    search_input_key_press = (e) => {
+        let key = e.charCode || e.which || e.keyCode || 0
+        if (key === 13)
+            this.looking_for()
     }
 
     looking_for = () => {
@@ -17,12 +24,20 @@ export default class Navbar extends Component {
 
     render() {
         return (
-            <div>
-                <input className="search-input" type="text" />
-                <button onClick={this.looking_for}>search</button>
-                <hr />
-                {(this.state.searching === "found") ? <Redirect to={'/search/' + this.state.looking_for} /> : null}
-
+            <div className="nav-bar">
+                <div className="logo">
+                    <a href="/">
+                        <img src={process.env.PUBLIC_URL + "/letra-logo.png"} alt="letra logo" />
+                        <span>etra</span>
+                    </a>
+                </div>
+                <div className="searching-form">
+                    <input className="search-input" type="text" onKeyPress={this.search_input_key_press} />
+                    <button onClick={this.looking_for}>
+                        <Loupe />
+                    </button>
+                    {(this.state.searching === "found") ? <Redirect to={'/search/' + this.state.looking_for} /> : null}
+                </div>
             </div>
         )
     }
