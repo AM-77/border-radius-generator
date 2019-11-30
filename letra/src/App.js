28f6ffdf21a100ad39884d413b465efc0d2e1589
AM-77 <amine.griche77@gmail.com>
2019-11-30 17:14:55 +0100

    added Footer, ListArtists components and the wrapper class

--- letra/src/App.js
+++ letra/src/App.js
@@ -1,28 +1,31 @@
-import React, { Component } from 'react'
+import React, { Component } from "react"
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 import Home from "./components/Home"
 import Lyrics from "./components/Lyrics"
 import Artist from "./components/Artist"
 import Navbar from "./components/Navbar"
-import Search from './components/Search'
+import Search from "./components/Search"
 import NotFound from "./components/NotFound"
-
-import AppContext from "./AppContext";
+import ListArtists from "./components/ListArtists"
+import AppContext from "./AppContext"
+import Footer from "./components/Footer"
 
 class App extends Component {
   render() {
     return (
       <AppContext>
         <Router>
-          <div><Navbar /></div>
-          <div>
+          <Navbar />
+          <div className="wrapper">
             <Switch>
               <Route exact path="/" component={Home} />
               <Route path="/artist/:artist" component={Artist} />
+              <Route path="/artists/:artist" component={ListArtists} />
               <Route path="/lyrics/:artist/:track" component={Lyrics} />
               <Route path="/search/:looking_for" component={Search} />
               <Route component={NotFound} />
             </Switch>
+            <Footer />
           </div>
         </Router>
 
