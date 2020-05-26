a15775d7f67533a10bb74bf0992bf3f0799f0643
AM-77 <amine.griche77@gmail.com>
2020-05-26 15:28:56 +0200

    Updates

---
+++ precache-manifest.cca030cdad9c66a8bc1feeda7be8635a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac0a6a00ae0683620496b7c846ad833b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "47b099fd73177af3d2c8",
+    "url": "/static/css/main.62143866.chunk.css"
+  },
+  {
+    "revision": "7d0228d7de7cb106555c",
+    "url": "/static/js/2.d710628d.chunk.js"
+  },
+  {
+    "revision": "47b099fd73177af3d2c8",
+    "url": "/static/js/main.af8ad052.chunk.js"
+  },
+  {
+    "revision": "9ac5f6079157535ae3ab",
+    "url": "/static/js/runtime-main.4ead8bd3.js"
+  },
+  {
+    "revision": "76e148cd5d93c912e75a02ae4b96bed0",
+    "url": "/static/media/loupe.76e148cd.svg"
+  }
+]);
