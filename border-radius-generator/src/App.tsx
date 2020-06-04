92e11783eafbc3fe73269fc47f7ea32c1e0612bd
AM-77 <amine.griche77@gmail.com>
2020-06-04 16:09:36 +0200

    add border-radius-generator app

---
+++ border-radius-generator/src/App.tsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+export default class App extends Component <unknown> {
+
+  constructor (props: unknown) {
+    super(props)
+    this.state = { }
+  }
+
+  render() {
+    const {  } = this.state
+    
+    return ( 
+      <div className="container">
+        
+      </div>
+      )
+  }
+}
