984b2105f9b28f25ad1735a6129b89306027b662
AM-77 <amine.griche77@gmail.com>
2020-06-04 22:24:06 +0200

    add component

---
+++ border-radius-generator/src/components/FullControl.tsx
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+import InputRange from './InputRange'
+
+interface IProps {
+  onRangeChange: (border: 'left' | 'top' | 'right' | 'bottom' | 'leftOp' | 'topOp' | 'rightOp' | 'bottomOp' , value: number) => void
+  width: number
+  height: number
+  left: number
+  leftOp: number
+  top: number
+  topOp: number
+  right: number
+  rightOp: number
+  bottom: number
+  bottomOp: number
+}
+
+
+export default class FullControl extends Component<IProps> {
+  render() {
+    const { width, height, onRangeChange, left, leftOp, top, topOp, right, rightOp, bottom, bottomOp } = this.props
+    return (
+      <div className="box-container">
+        <div style={{ width, height }}  className="box">
+          <div className="left" style={{ width: height }} ><InputRange onRangeChange={(value) => onRangeChange('left', value)} /></div>
+          <div className="left op" style={{ width: height }} ><InputRange onRangeChange={(value) => onRangeChange('leftOp', value)} /></div>
+          <div className="top" style={{ width: width }} ><InputRange onRangeChange={(value) => onRangeChange('top', value)} /></div>
+          <div className="top op" style={{ width: width }} ><InputRange onRangeChange={(value) => onRangeChange('topOp', value)} /></div>
+          <div className="right" style={{ width: height }} ><InputRange onRangeChange={(value) => onRangeChange('right', value)} /></div>
+          <div className="right op" style={{ width: height }} ><InputRange onRangeChange={(value) => onRangeChange('rightOp', value)} /></div>
+          <div className="bottom" style={{ width: width }} ><InputRange onRangeChange={(value) => onRangeChange('bottom', value)} /></div>
+          <div className="bottom op" style={{ width: width }} ><InputRange onRangeChange={(value) => onRangeChange('bottomOp', value)} /></div>
+          <div className="inner-box" style={{ borderRadius: `${topOp}% ${top}% ${bottomOp}% ${bottom}% / ${left}% ${rightOp}% ${right}% ${leftOp}%` }} ></div>
+        </div>
+      </div>
+    )
+  }
+}
