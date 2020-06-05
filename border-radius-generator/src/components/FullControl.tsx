9f44f5e4bfa96e700ffd3308e5eb788212e9db4b
AM-77 <amine.griche77@gmail.com>
2020-06-05 21:46:10 +0200

    add input range default value

--- border-radius-generator/src/components/FullControl.tsx
+++ border-radius-generator/src/components/FullControl.tsx
@@ -15,21 +15,20 @@ interface IProps {
   bottomOp: number
 }
 
-
 export default class FullControl extends Component<IProps> {
   render() {
     const { width, height, onRangeChange, left, leftOp, top, topOp, right, rightOp, bottom, bottomOp } = this.props
     return (
       <div className="box-container">
         <div style={{ width, height }}  className="box">
-          <div className="left" style={{ width: height }} ><InputRange onRangeChange={(value) => onRangeChange('left', value)} /></div>
-          <div className="left op" style={{ width: height }} ><InputRange onRangeChange={(value) => onRangeChange('leftOp', value)} /></div>
-          <div className="top" style={{ width: width }} ><InputRange onRangeChange={(value) => onRangeChange('top', value)} /></div>
-          <div className="top op" style={{ width: width }} ><InputRange onRangeChange={(value) => onRangeChange('topOp', value)} /></div>
-          <div className="right" style={{ width: height }} ><InputRange onRangeChange={(value) => onRangeChange('right', value)} /></div>
-          <div className="right op" style={{ width: height }} ><InputRange onRangeChange={(value) => onRangeChange('rightOp', value)} /></div>
-          <div className="bottom" style={{ width: width }} ><InputRange onRangeChange={(value) => onRangeChange('bottom', value)} /></div>
-          <div className="bottom op" style={{ width: width }} ><InputRange onRangeChange={(value) => onRangeChange('bottomOp', value)} /></div>
+          <div className="left" style={{ width: height }} ><InputRange value={left} onRangeChange={(value) => onRangeChange('left', value)} /></div>
+          <div className="left op" style={{ width: height }} ><InputRange value={leftOp} onRangeChange={(value) => onRangeChange('leftOp', value)} /></div>
+          <div className="top" style={{ width: width }} ><InputRange value={top} onRangeChange={(value) => onRangeChange('top', value)} /></div>
+          <div className="top op" style={{ width: width }} ><InputRange value={topOp} onRangeChange={(value) => onRangeChange('topOp', value)} /></div>
+          <div className="right" style={{ width: height }} ><InputRange value={right} onRangeChange={(value) => onRangeChange('right', value)} /></div>
+          <div className="right op" style={{ width: height }} ><InputRange value={rightOp} onRangeChange={(value) => onRangeChange('rightOp', value)} /></div>
+          <div className="bottom" style={{ width: width }} ><InputRange value={bottom} onRangeChange={(value) => onRangeChange('bottom', value)} /></div>
+          <div className="bottom op" style={{ width: width }} ><InputRange value={bottomOp} onRangeChange={(value) => onRangeChange('bottomOp', value)} /></div>
           <div className="inner-box" style={{ borderRadius: `${topOp}% ${top}% ${bottomOp}% ${bottom}% / ${left}% ${rightOp}% ${right}% ${leftOp}%` }} ></div>
         </div>
       </div>
