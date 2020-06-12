392e227f0635a6ed25a9a0c0cff37cbc72def821
AM-77 <amine.griche77@gmail.com>
2020-06-12 20:05:32 +0200

    add footer

---
+++ border-radius-generator/src/components/Footer.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <p>Made with <span>&lt;/&gt;</span> by <a href="github.com/AM-77">Med Amine</a> @<span>AM-77</span></p>
+  )
+}
