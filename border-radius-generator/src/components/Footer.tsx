51511e0502d3266842ec89615dfd6f98fe60ac7f
AM-77 <amine.griche77@gmail.com>
2020-06-12 20:07:42 +0200

    fix link

--- border-radius-generator/src/components/Footer.tsx
+++ border-radius-generator/src/components/Footer.tsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <p>Made with <span>&lt;/&gt;</span> by <a href="github.com/AM-77">Med Amine</a> @<span>AM-77</span></p>
+    <p>Made with <span>&lt;/&gt;</span> by <a href="https://www.github.com/AM-77">Med Amine</a> @<span>AM-77</span></p>
   )
 }
